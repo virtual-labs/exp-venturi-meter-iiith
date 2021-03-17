56f40087d9d470ff694d9c1ace1fa880c9b7512d
Ar9020th <arthraj9@gmail.com>
2021-03-17 18:07:09 +0530

    responsiveness increased

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -2,71 +2,68 @@ let sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
-ids = [
-  "horizontal-tube",
-];
-tube_ids = [
-	"first-tube",
-	"second-tube",
-	];
+ids = ["horizontal-tube"];
+tube_ids = ["first-tube", "second-tube"];
 
 let asyncMove = async (id, curPosition = 0, finalPosition = 1) => {
-	let path = document.getElementById(id);
-	let flags = [true, true];
-	while (true) {
-	  if (curPosition > finalPosition) break;
-	  curPosition += 0.01;
-	  path.setAttribute("offset", curPosition);
-	  await sleep(0.5);
-	}
+  speed2 = document.getElementById("water-flow").value;
+  speed2 = speed2 * 0.0002;
+  speed2 = speed2 == 0 ? 0.0002 : speed2;
+  let path = document.getElementById(id);
+  let flags = [true, true];
+  while (true) {
+    if (curPosition > finalPosition) break;
+    curPosition += speed2;
+    path.setAttribute("offset", curPosition);
+    await sleep(0.5);
+  }
 };
 
 let startAnimation = async () => {
-	for (let i = 0; i < ids.length; i++) {
-	  id = ids[i];
-	  let path = document.getElementById(id);
-	  flags = [true, true];
-	  let finalPosition = 1;
-	  let curPosition = 0;
-	  while (true) {
-		if (id == "horizontal-tube") {
-			if(curPosition > 0.05 && flags[0]) {
-				asyncMove("first-tube");
-				flags[0] = false
-			}
-			if(curPosition > 0.55 && flags[1]) {
-				asyncMove("second-tube");
-				flags[1] = false
-			}
-		}
-		if (curPosition > finalPosition) break;
-		curPosition += 0.01;
-		path.setAttribute("offset", curPosition);
-		await sleep(0.5);
-	  }
-	}
-  };
-  
-  let resetEverything = () => {
-	tube_ids.forEach((element) => {
-	  let path = document.getElementById(element);
-	  path.setAttribute("offset", 0);
-	});
-	ids.forEach((ele) => {
-	  let path = document.getElementById(ele);
-	  path.setAttribute("offset", 0);
-	});
-  };
-  disablestart = false;
-  let startAn = async () => {
-	resetEverything();
-	document.getElementById("startbutton").className = "button disabled";
-	document.getElementById("resetbutton").className = "button disabled";
-	document.getElementById("startbutton").disabled = true;
-	document.getElementById("resetbutton").disabled = true;
-	await startAnimation();
-	document.getElementById("startbutton").className = "button";
-	document.getElementById("resetbutton").className = "button";
-	document.getElementById("startbutton").disabled = false;
-	document.getElementById("resetbutton").disabled = false;
-  };
+  speed2 = document.getElementById("water-flow").value;
+  speed2 = speed2 * 0.0002;
+  speed2 = speed2 == 0 ? 0.0002 : speed2;
+  for (let i = 0; i < ids.length; i++) {
+    id = ids[i];
+    let path = document.getElementById(id);
+    flags = [true, true];
+    let finalPosition = 1;
+    let curPosition = 0;
+    while (true) {
+      if (id == "horizontal-tube") {
+        if (curPosition > 0.05 && flags[0]) {
+          asyncMove("first-tube");
+          flags[0] = false;
+        }
+        if (curPosition > 0.55 && flags[1]) {
+          asyncMove("second-tube");
+          flags[1] = false;
+        }
+      }
+      if (curPosition > finalPosition) break;
+      curPosition += speed2;
+      path.setAttribute("offset", curPosition);
+      await sleep(0.5);
+    }
+  }
+};
+
+let resetEverything = () => {
+  tube_ids.forEach((element) => {
+    let path = document.getElementById(element);
+    path.setAttribute("offset", 0);
+  });
+  ids.forEach((ele) => {
+    let path = document.getElementById(ele);
+    path.setAttribute("offset", 0);
+  });
+};
+disablestart = false;
+let startAn = async () => {
+  resetEverything();
+  document.getElementById("startbutton").disabled = true;
+  document.getElementById("resetbutton").disabled = true;
+  await startAnimation();
+  document.getElementById("startbutton").disabled = false;
+  document.getElementById("resetbutton").disabled = false;
+};
